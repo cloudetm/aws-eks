50064cd04c49bb6dc73360f37184ac20ba8280ba
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-03-17 11:33:02 +0530

    getting the latest amazon linux 2 ami using `aws_ami datasource`

--- terraform/bastian-instance.tf
+++ terraform/bastian-instance.tf
@@ -15,3 +15,28 @@ module "bastian_instance_security_group" {
         module.vpc
     ]
 }
+
+data "aws_ami" "amazon_linux_2" {
+    most_recent = true
+    owners = [ "amazon" ]
+
+    filter {
+        name = "name"
+        values = [ "amzn2-ami-hvm-*-gp2" ]
+    }
+
+    filter {
+        name = "root-device-type"
+        values = [ "ebs" ]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = [ "hvm" ]
+    }
+
+    filter {
+        name = "architecture"
+        values = [ "x86_64" ]
+    }
+}
