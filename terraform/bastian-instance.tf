b3422c4fdc6ce0a58a6bde0f3a70645195387dd2
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-03-17 11:39:54 +0530

    correction

--- terraform/bastian-instance.tf
+++ terraform/bastian-instance.tf
@@ -2,7 +2,7 @@ module "bastian_instance_security_group" {
     source = "terraform-aws-modules/security-group/aws"
     version = "4.7.0"
 
-    name = "bastian-instance"
+    name = "bastian-instance-security-group"
     description = "Security group for bastian instance with SSH port open to the internet"
     vpc_id = module.vpc.vpc_id
 
